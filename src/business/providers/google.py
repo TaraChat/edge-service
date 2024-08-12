00857c528c2a051b05d6bbebe6e677378547f68b
Thiago Martins <thiago.fmartins@outlook.com>
2024-08-12 20:53:21 -0300

    Update API_SECRET env var name

--- src/business/providers/google.py
+++ src/business/providers/google.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 
-genai.configure(api_key=os.environ["GEMINI_SECRET_KEY"])
+genai.configure(api_key=os.environ["GOOGLE_SECRET_KEY"])
 
 
 class GoogleProvider:
